f334bcd556c27f1b29f15e783158992d48a9902e
github username <github email address>
2019-11-23 17:07:57 -0500

    Done.

--- index.js
+++ index.js
@@ -1,5 +1,84 @@
 /* Your Code Here */
 
+let createEmployeeRecord = function(employeeData) {
+    let name = employeeData[0]
+    let lastName = employeeData[1]
+    let job = employeeData[2]
+    let pay = employeeData[3]
+
+    let employeeRecord = {
+        firstName: name,
+        familyName: lastName,
+        title: job,
+        payPerHour: pay,
+        timeInEvents: [],
+        timeOutEvents: []
+    }
+    return employeeRecord
+}
+
+let createEmployeeRecords = function(allEmployeeData) {
+    let employeeRecords = allEmployeeData.map((employee) => createEmployeeRecord(employee))
+    return employeeRecords
+}
+
+let createTimeInEvent = function(event) {
+   // split the event array, convert the hour to an integer
+   let eventArr = event.split(' ')
+   let shiftDate = eventArr[0]
+   let shiftHour = parseInt(eventArr[1])
+
+   // we now have the elements of the new time card, this needs to be pushed into the EE record
+   this.timeInEvents.push({
+      type: "TimeIn",
+      date: shiftDate,
+      hour: shiftHour
+   })
+    // return this
+   return this
+}
+
+let createTimeOutEvent = function(event) {
+    // split the event array, convert the hour to an integer
+   let eventArr = event.split(' ')
+   let shiftDate = eventArr[0]
+   let shiftHour = parseInt(eventArr[1])
+
+   this.timeOutEvents.push({
+    type: "TimeOut",
+    date: shiftDate,
+    hour: shiftHour
+   })
+   return this
+}
+
+let hoursWorkedOnDate = function(desiredDate) {
+    let inEvent = this.timeInEvents.find(function(e) {
+        return e.date === desiredDate
+    })
+
+    let outEvent = this.timeOutEvents.find(function(e) {
+        return e.date === desiredDate
+    })
+    return (outEvent.hour - inEvent.hour) / 100
+}
+
+let wagesEarnedOnDate = function(desiredDate) {
+    let total = hoursWorkedOnDate.call(this, desiredDate) * this.payPerHour
+
+    return parseFloat(total.toString())
+}
+
+let calculatePayroll = function(arrayOfEmployeeRecords){
+    return arrayOfEmployeeRecords.reduce(function(memo, rec){
+        return memo + allWagesFor.call(rec)
+    }, 0)
+}
+
+let findEmployeeByFirstName = function(employees, searchName) {
+    return employees.find((employee) => employee.firstName === searchName)
+}
+
 /*
  We're giving you this function. Take a look at it, you might see some usage
  that's new and different. That's because we're avoiding a well-known, but
@@ -20,3 +99,4 @@ let allWagesFor = function () {
 
     return payable
 }
+
