45be851d59e7083b59f8a929ca605c9dd695d4cf
PineappleGuy <garff247@gmail.com>
2020-02-11 14:54:08 -0800

    Done.

--- index.js
+++ index.js
@@ -9,6 +9,63 @@
  for you to use if you need it!
  */
 
+class Employee {
+    constructor(array) {
+        this.firstName = array[0];
+        this.familyName = array[1];
+        this.title = array[2];
+        this.payPerHour = array[3];
+        this.timeInEvents = [];
+        this.timeOutEvents = [];
+    }
+    
+}
+
+function createEmployeeRecord(array) {
+    return new Employee(array)
+}
+
+function createEmployeeRecords(array) {
+    return array.map(function(element) {
+        return createEmployeeRecord(element)
+    })
+}
+
+function createTimeInEvent(dateStamp) {
+    let stamp = dateStamp.split(' ')
+    let event = {}
+    event.type = "TimeIn"
+    event.hour = parseInt(stamp[1])
+    event.date = stamp[0]
+    this.timeInEvents.push(event)
+    return this
+}
+
+function createTimeOutEvent(dateStamp) {
+    let stamp = dateStamp.split(' ')
+    let event = {}
+    event.type = "TimeOut"
+    event.hour = parseInt(stamp[1])
+    event.date = stamp[0]
+    this.timeOutEvents.push(event)
+    return this
+}
+
+function hoursWorkedOnDate(date) {
+    let i = this.timeInEvents.find(e => e.date == date)
+    let o = this.timeOutEvents.find(e => e.date == date)
+    return (o.hour - i.hour) / 100
+}
+
+function wagesEarnedOnDate(date) {
+    let h = hoursWorkedOnDate.bind(this)
+    return h(date) * this.payPerHour
+}
+
+function findEmployeeByFirstName(array, name) {
+    return array.find(element => element.firstName == name)
+}
+
 let allWagesFor = function () {
     let eligibleDates = this.timeInEvents.map(function (e) {
         return e.date
@@ -19,4 +76,10 @@ let allWagesFor = function () {
     }.bind(this), 0) // <== Hm, why did we need to add bind() there? We'll discuss soon!
 
     return payable
+}
+
+function calculatePayroll(array) {
+    return array.reduce(function(total, employee) {
+        return total + allWagesFor.call(employee)
+    }, 0)
 }
